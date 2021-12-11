9d6cb841e8d5d15f2d9640d78011a7853a37d497
eminsakk <63427668+eminsakk@users.noreply.github.com>
2021-12-12 00:58:41 +0300

    Update radix-sort.h

--- Radix Sort/radix-sort.h
+++ Radix Sort/radix-sort.h
@@ -1,6 +1,8 @@
 #ifndef RADIX-SORT_H
 #define RADIX-SORT_H
 
+int encode(int l,std::string str,int currStrIndex);
+void subRoutine(std::string arr[], int size, int currStrIndex, int l,int &val);
 int radixSort(std::string arr[], bool ascending, int n, int l);
 
 #endif
