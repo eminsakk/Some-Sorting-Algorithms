b9e660396d7775d361573241a416cedcdc7a2f4e
eminsakk <63427668+eminsakk@users.noreply.github.com>
2021-12-29 23:06:21 +0300

    Update test.cpp

--- Quick Sort/test.cpp
+++ Quick Sort/test.cpp
@@ -4,7 +4,6 @@
 #include <ctime>
 #include "qs.h"
 
-//the2.h only contains declaration of the function quickSort  and quickSort3 which are:
 //void quickSort(unsigned short* arr, long &swap, double & avg_dist, double & max_dist, bool hoare, int size);
 //void quickSort3(unsigned short *arr, long &swap, long &comparison, int size);
 
